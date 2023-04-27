1d4394952d1c0a45b2bcbafd764cdf6ba053fa55
Melenik Inna <dteamaker@gmail.com>
2023-04-28 00:52:03 +0300

    style fix

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('silk/', include('silk.urls', namespace='silk')),
     path('', include("menu.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [path('silk/', include('silk.urls', namespace='silk'))]\
+                  + urlpatterns
